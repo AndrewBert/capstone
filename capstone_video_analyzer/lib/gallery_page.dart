5b663a8555e3a633c19d12231d46edaf6b57dd86
Andrew <bertina@sunypoly.edu>
2021-04-08 21:22:57 -0400

    thumbnails load properly, uploading

--- capstone_video_analyzer/lib/gallery_page.dart
+++ capstone_video_analyzer/lib/gallery_page.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
+import 'package:capstone_video_analyzer/analysis_results_class.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'auth_service.dart';
 
@@ -13,32 +18,97 @@ class GalleryPage extends StatefulWidget {
 }
 
 class _GalleryPageState extends State<GalleryPage> {
+  Future? thumbnailFuture;
+  List<CachedNetworkImage> thumbnails = [];
+  User? currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = context.read<AuthService>().currentUser;
+    thumbnailFuture = _getThumbnails();
+  }
   //download all video thumbnails
-  List<Image> thumbnails = [];
+  // List<AnalysisResults> analysisResults = [];
 
   //display thumbnails in gallery
 
   //open video player on thumbnail pressed
 
-  Future<void> _getThumbnails(BuildContext context) async {
-    User? currentUser = context.read<AuthService>().currentUser;
-    if (currentUser == null) return;
+  _getThumbnails() async {
+    if (currentUser == null) return [];
 
     var thumbnailStorage =
         FirebaseStorage.instanceFor(bucket: "gs://thumbnails5555")
             .ref()
-            .child(currentUser.uid);
+            .child(currentUser!.uid);
 
     var result = await thumbnailStorage.listAll();
 
-     result.items.forEach((imageReference) async {
-        var downloadUrl = await imageReference.getDownloadURL();
-        var thumbnail = Image.network(
-          downloadUrl.toString(),
-          fit: BoxFit.scaleDown,
-        );
-        thumbnails.add(thumbnail);
-      });
+    for (var imageRef in result.items) {
+      var downloadUrl = await imageRef.getDownloadURL();
+      var thumbnail = CachedNetworkImage(
+        imageUrl: downloadUrl.toString(),
+        fit: BoxFit.scaleDown,
+        placeholder: (context, url) => CircularProgressIndicator(),
+      );
+      thumbnails.add(thumbnail);
+    }
+  }
+
+  Future<void> _getLabels(BuildContext context) async {
+    User? currentUser = context.read<AuthService>().currentUser;
+    if (currentUser == null) return;
+
+    var labelStorage =
+        FirebaseStorage.instanceFor(bucket: "gs://analysis_json5555")
+            .ref()
+            .child(currentUser.uid);
+
+    var result = await labelStorage.listAll();
+
+    result.items.forEach((labelsJson) async {
+      var downloadUrl = await labelsJson.getDownloadURL();
+      //TODO parse json into AnlaysisResults
+    });
+  }
+
+  /// The user selects a file, and the task is added to the list.
+  Future<UploadTask?> uploadFile(PickedFile? file, BuildContext context) async {
+    if (file == null) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('No file was selected'),
+      ));
+      return null;
+    }
+
+    User? currentUser = context.read<AuthService>().currentUser;
+    //TODO throw proper exception instead?
+    if (currentUser == null) return null;
+
+    UploadTask uploadTask;
+
+    // Create a Reference to the file
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child(currentUser.uid)
+        .child('/${path.basename(file.path)}');
+
+    final metadata = SettableMetadata(
+        contentType: 'video/mp4',
+        customMetadata: {'picked-file-path': file.path});
+
+    uploadTask = ref.putFile(File(file.path), metadata);
+
+    _getThumbnails();
+
+    return Future.value(uploadTask);
+  }
+
+  Future<void> selectVideoForUpload(BuildContext context) async {
+    PickedFile? file =
+        await ImagePicker().getVideo(source: ImageSource.gallery);
+    await uploadFile(file, context);
   }
 
   @override
@@ -47,18 +117,23 @@ class _GalleryPageState extends State<GalleryPage> {
       appBar: AppBar(
         title: Text("Gallery"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => selectVideoForUpload(context),
+        child: Icon(Icons.add),
+      ),
       body: FutureBuilder(
-        future: _getThumbnails(context),
+        future: thumbnailFuture,
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return Container(
                 child: GridView.builder(
                     itemCount: thumbnails.length,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
+                        crossAxisCount: 3),
                     itemBuilder: (BuildContext context, int index) {
                       final thumbnailItem = thumbnails[index];
                       return Container(
+                        color: Colors.black,
                         child: thumbnailItem,
                       );
                     }));
@@ -70,7 +145,6 @@ class _GalleryPageState extends State<GalleryPage> {
                 width: MediaQuery.of(context).size.width / 1.25,
                 child: CircularProgressIndicator());
           }
-
           return Container();
         },
       ),
