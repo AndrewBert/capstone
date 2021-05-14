1af98e624cdfd924c79e284d245bdfcf73ddfb37
Andrew <bertina@sunypoly.edu>
2021-05-13 21:38:49 -0400

    attempt

--- capstone_video_analyzer/lib/pages/signIn_page.dart
+++ capstone_video_analyzer/lib/pages/signIn_page.dart
@@ -10,30 +10,66 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                          'Welcome! Please sign in to search, view, or upload your videos.',
+                          style: TextStyle(fontSize: 24),textAlign: TextAlign.center,),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: TextField(
+                              controller: emailController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Email",
+                              ),
+                            ),
+                          ),
+                          TextField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Password",
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                context.read<AuthService>().signIn(
+                                      email: emailController.text.trim(),
+                                      password: passwordController.text.trim(),
+                                    );
+                              },
+                              child: Text("Sign in"),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: "Password",
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.read<AuthService>().signIn(
-                      email: emailController.text.trim(),
-                      password: passwordController.text.trim(),
-                    );
-              },
-              child: Text("Sign in"),
-            )
-          ],
+          ),
         ),
       ),
     );
