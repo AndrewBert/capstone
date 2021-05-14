4a70dffcfb2570f83e5c79428ddc732ef3b1b6af
Andrew <bertina@sunypoly.edu>
2021-05-13 21:38:28 -0400

    divider in category view

--- capstone_video_analyzer/lib/widgets/category_list.dart
+++ capstone_video_analyzer/lib/widgets/category_list.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 import 'thumbnail_card.dart';
 
-
 class CategoryList extends StatelessWidget {
   final Function(String) onDeleteVideo;
   final List<Category> categories;
@@ -15,35 +14,44 @@ class CategoryList extends StatelessWidget {
     categories.sort(
         (b, a) => a.videoDataList.length.compareTo(b.videoDataList.length));
     return Container(
-      child: ListView.builder(
-          itemCount: categories.length,
-          itemBuilder: (BuildContext context, int index) {
-            final videoDataList = categories[index].videoDataList;
-            return Column(
-              children: [
-                Text(categories[index].name.toUpperCase()),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  height: 240,
-                  child: ListView.builder(
-                    physics: ClampingScrollPhysics(),
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: videoDataList.length,
-                    itemBuilder: (BuildContext context, int i) {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 1),
-                        child: AspectRatio(
-                            aspectRatio: 9 / 16,
-                            child: ThumbCard(
-                                videoDataList[i], onDeleteVideo)),
-                      );
-                    },
-                  ),
+      child: ListView.separated(
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          final videoDataList = categories[index].videoDataList;
+          return Column(
+            children: [
+              Text(
+                categories[index].name.toUpperCase(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                height: 240,
+                child: ListView.builder(
+                  physics: ClampingScrollPhysics(),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: videoDataList.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 1),
+                      child: AspectRatio(
+                          aspectRatio: 9 / 16,
+                          child: ThumbCard(videoDataList[i], onDeleteVideo)),
+                    );
+                  },
                 ),
-              ],
-            );
-          }),
+              ),
+            ],
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            color: Colors.black87,
+            thickness: 5,
+          );
+        },
+      ),
     );
   }
 }
